f33399f7938349732f37bec5d2015e0ba8c7c426
supakornbabe <supakornbabew@hotmail.com>
2016-05-26 14:18:10 +0700

    Revert "revert"
    This reverts commit 2f818ec3f47e43c1a6dd5895be5633b422458c9c.

--- babe.js
+++ babe.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
     $('#btn1').click(function() {
         var button='<button class="itembtn">' + "Complete" + '</button>' ;
         $('.list').append(button);
-        var textadd='<p class="item">' + $('input[name=checkListItem]').val()+'</p><br/><br/>';
+        var textadd='<p class="item">' + $('input[name=checkListItem]').val()  + '</p>'+'<br>'+'<br>';
         $('.list').append(textadd);
-                $(document).on('click', '.itembtn', function() {
+        $(document).on('click', '.itembtn', function() {
             $(this).nextUntil("button").remove();
             $(this).remove();
         });
