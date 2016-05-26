492b18a4c5f672d9468498a7a62aedbe199d0973
supakornbabe <supakornbabew@hotmail.com>
2016-05-26 13:36:34 +0700

    edit js

--- babe.js
+++ babe.js
@@ -4,8 +4,7 @@ $(document).ready(function() {
         $('.list').append(button);
         var textadd='<p class="item">' + $('input[name=checkListItem]').val()+'</p><br/><br/>';
         $('.list').append(textadd);
-        $('.list').addClass(abc);
-        $(document).on('click', '.itembtn', function() {
+                $(document).on('click', '.itembtn', function() {
             $(this).nextUntil("button").remove();
             $(this).remove();
         });
